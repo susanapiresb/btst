5e7c3df3afd97eb5477f5d929440ba3ccaee0382
susanapiresb <111321424+susanapiresb@users.noreply.github.com>
2022-08-16 17:50:58 +0100

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -3,7 +3,6 @@ session_start();
 include('verifica.php');
 $str = "dbname=ddvv0meaeaknjq user=ziytsfqigzzvhy password=6c16f29cf98ff490a5b01096fd076a289e318d743a759ecbd41e5db3fd0faa82 host=ec2-54-194-211-183.eu-west-1.compute.amazonaws.com port=5432";
 $connection = pg_connect($str);
-return $connection;
 
 $username = $_SESSION['username'];
 
