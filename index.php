120ee634f0d857c32b67a2b494254f19c563558e
susanapiresb <111321424+susanapiresb@users.noreply.github.com>
2022-08-16 12:09:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$connection = pg_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$str = "dbname=postgres user=postgres password=postgres host=localhost port=5432";
+$connection = pg_connect($str);
 
 if(isset($_POST['entrar'])) {
     session_start();
