e4198f130c5f81f3fbaf0120fa824e5c90ce1228
susanapiresb <111321424+susanapiresb@users.noreply.github.com>
2022-08-16 18:18:55 +0100

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -19,16 +19,16 @@ if (isset($_POST['adicionar'])) {
     move_uploaded_file($_FILES["arquivo"]["tmp_name"], $target_dir.$_FILES['arquivo']['name']);
 
     //para verificar que nao há outro filme igual
-    $query = "SELECT codigo FROM ziytsfqigzzvhy@heroku.btstpeca WHERE codigo = '$codigo'";
+    $query = "SELECT codigo FROM btstpeca WHERE codigo = '$codigo'";
     $resultado = pg_query($connection, $query);
 
 
-    if (pg_affected_rows($resultado) != 0){    //se essa peça ja foi adicionada
+    if (pg_affected_rows($resultado) == 1){    //se essa peça ja foi adicionada
         $erro = "Peça já adicionada";
         //header('location: index.php');
     }
     else {   //se essa peça ainda não foi adicionada, coloca-a na base de dados
-        $query1 = "INSERT INTO ziytsfqigzzvhy@heroku.btstpeca (codigo, nome, imagem, concluida, btsttrabalhador_username, quantidade, prazoentrega) VALUES ('$codigo','$nome','$image',false, '$username', '$quantidade', '$prazoentrega')";
+        $query1 = "INSERT INTO btstpeca (codigo, nome, imagem, concluida, btsttrabalhador_username, quantidade, prazoentrega) VALUES ('$codigo','$nome','$image',false, '$username', '$quantidade', '$prazoentrega')";
         $resultado1 = pg_query($connection, $query1);
         $erro = "Peça adicionada com sucesso!";
         //header('location: menu.php');
